c9f52eeea33486998578ea63c1079c693df57eb9
LenaDutova <lena.dutova@list.ru>
2023-01-24 13:15:34 +0300

    update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pylab
 import os
 
 for filename in os.listdir('data'):
-# filename = 'OPPO_CPH1931_2022-12-06T10 55 14.686Z.csv'
+    # filename = 'OPPO_CPH1931_2022-12-06T10 55 14.686Z.csv'
     file = pd.read_csv('data/' + filename)
     x = file['X'].tolist()
     y = file['Y'].tolist()
@@ -14,16 +14,13 @@ for filename in os.listdir('data'):
 
     color_map = plt.get_cmap('cividis')
 
-    for k in [x, y, z]:
-        i = 0
-
+    for k, i in zip([x, y, z], ['x', 'y', 'z']):
         fig = plt.figure(figsize=(12, 8))
         axs = fig.add_subplot(111, projection='3d')
-        scatter_plot = axs.scatter3D(x, y, z, c=k, cmap = color_map)
+        scatter_plot = axs.scatter3D(x, y, z, c=k, cmap=color_map)
         axs.set_xlabel('X Label')
         axs.set_ylabel('Y Label')
         axs.set_zlabel('Z Label')
         plt.colorbar(scatter_plot)
         plt.savefig('images/' + filename + '_' + str(i) + '.png')
-        i = i + 1
         plt.show()
